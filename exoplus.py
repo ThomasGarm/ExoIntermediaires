b83ad633c39aa0de07c31ead472d24895749f641
python <thomas.win@gmail.com>
2019-10-17 16:38:23 +0200

    find my no inpuuuuut lalala

--- exoplus.py
+++ exoplus.py
@@ -7,3 +7,17 @@ if a>b and a>c and a>d:
     print (a)
 
 # ex2 condition d'âge
+import math
+age = int(input("entrez votre âge"))
+if age<0:
+    print ("pas possib'")
+if age>=21:
+    print ("authorized access")
+if age % 2 == 0:
+    print ("even age")
+if math.sqrt(age).is_integer():
+    print ("square")
+if math.sqrt(age).is_integer()==False:
+    print ("not a square")
+else:
+    print ("no input")
