d45a624839b263ad88333d6c196f4d4585285df8
python <thomas.win@gmail.com>
2019-10-23 16:53:25 +0200

    commit for commit

---
+++ Exoplus2.py
@@ -0,0 +1,15 @@
+#Exercice1: un échiquier.
+echec="#"
+i=0
+liste=[1,0,1,0,1,0,1,0,1]
+if i==1:
+    print(echec)
+
+
+
+#exercice2: Matrix dans le terminal
+list=[0, 0, 0, 0]
+for i in range(0, 4):
+    list[i]=1
+    print ("{}".format(list[0]), list[1])
+    list[i]=0
